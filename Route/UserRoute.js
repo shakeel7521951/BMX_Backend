f685c7689dd883a7794811b35d5a038cdcf01319
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-03-27 15:00:46 +0500

    push for deploy

---
+++ Route/UserRoute.js
@@ -0,0 +1,50 @@
+import express from "express";
+import {
+  addFeedback,
+  convertPoints,
+  convertReferredPoints,
+  DailyClaim,
+  forgotPasswordOTP,
+  getallusers,
+  getReferredUserData,
+  investment,
+  Login,
+  Logout,
+  Myprofile,
+  resetPassword,
+  Signup,
+  updateEligibilityCriteria,
+  updatePass,
+  updateUserRole,
+  uploadPaymentImage,
+  verifyOTP,
+  verifyUser,
+} from "../Controller/UserController.js";
+import { isUserLoggedin } from "../Utils/Auth.js";
+import upload from "../Utils/multerConfig.js";
+
+const Router = express.Router();
+
+Router.post("/signup", Signup);
+Router.post("/verify-user", verifyUser);
+Router.post("/login", Login);
+Router.post("/logout", isUserLoggedin, Logout);
+Router.get("/profile", isUserLoggedin, Myprofile);
+Router.put("/updatePass", isUserLoggedin, updatePass);
+Router.get("/points", isUserLoggedin, DailyClaim);
+Router.get("/getRef", isUserLoggedin, getReferredUserData);
+Router.post("/feedBack", isUserLoggedin, addFeedback);
+Router.post("/investment", isUserLoggedin, investment);
+Router.put("/convert-points/:id", isUserLoggedin, convertPoints);
+Router.put("/refConvert-points/:id", isUserLoggedin, convertReferredPoints);
+Router.get("/users", isUserLoggedin, getallusers);
+
+Router.post("/forgot-password-otp",forgotPasswordOTP);
+Router.post("/verify-otp",verifyOTP);
+Router.put("/reset-password",resetPassword);
+
+Router.put("/upload-payment-image",isUserLoggedin,upload.single('paymentImage'),uploadPaymentImage);
+Router.put("/update-eligibility-criteria/:userId",isUserLoggedin,updateEligibilityCriteria);
+Router.put("/update-user-role/:userId",isUserLoggedin,updateUserRole);
+
+export default Router;
---
+++ server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectD from "./ConnectDb/ConnectDB.js";
+import Userroute from "./Route/UserRoute.js";
+import TaskRoute from "./Route/TaskRoute.js";
+import withdrawRoute from './Route/Withdraw.js';
+import Error from "./MiddleWare/Error.js";
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught exception: ${err.message}`);
+  process.exit(1);
+});
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.FRONT_END_URL,
+    credentials: true,
+  })
+);
+
+app.get('/',(req,res)=>{
+  res.send("Backend is running.......")
+})
+
+app.use(cookieParser());
+
+app.use("/api/v1", Userroute);
+app.use("/api/v1", TaskRoute);
+app.use("/api/v1", withdrawRoute);
+
+app.use(Error);
+
+const Server = app.listen(PORT, () => {
+  console.log(`Server is runing on ${PORT}`);
+  connectD();
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Server rejected");
+  console.error(`Unhandled Rejection: ${err.message}`);
+  Server.close(() => {
+    process.exit(1);
+  });
+});
