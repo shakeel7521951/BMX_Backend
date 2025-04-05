a8a91d21fee4c8ceab3d0038495d063c2d05e982
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-04-05 12:59:11 +0500

    update server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -14,9 +15,8 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.use(
